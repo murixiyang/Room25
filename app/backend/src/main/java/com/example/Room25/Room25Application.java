e694178b235644e2627f506b82ac4d56b3e07ecf
muxi <yangqinxue333@163.com>
2024-03-29 13:49:51 +0000

    Add backend

---
+++ app/backend/src/main/java/com/example/Room25/Room25Application.java
@@ -0,0 +1,13 @@
+package com.example.Room25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Room25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Room25Application.class, args);
+	}
+
+}
