effe611c9640a54673fe48987157a33185145426
muxi <yangqinxue333@163.com>
2024-04-07 12:48:12 +0100

    Add fields for room

--- Room25Web/src/app/room/room.component.ts
+++ Room25Web/src/app/room/room.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+// Define enums
+enum DangerousLevel {
+  GREEN = 'Grenn',
+  YELLOW = 'Yellow',
+  Red = 'Red',
+}
+
+enum LockStatus {
+  Available = 'Available',
+  Locked = 'Locked',
+  Destroyed = 'Destroyed',
+}
 
 @Component({
   selector: 'app-room',
   standalone: true,
   imports: [],
   templateUrl: './room.component.html',
-  styleUrl: './room.component.css'
+  styleUrl: './room.component.css',
 })
 export class RoomComponent {
+  dangerousLevel: DangerousLevel;
+  xIndex: number;
+  yIndex: number;
+  lockStatus: LockStatus;
 
+  constructor(
+    dangerousLevel: DangerousLevel,
+    xIndex: number,
+    yIndex: number,
+    lockStatus: LockStatus
+  ) {
+    this.dangerousLevel = dangerousLevel;
+    this.xIndex = xIndex;
+    this.yIndex = yIndex;
+    this.lockStatus = lockStatus;
+  }
+
+  // Placeholder function for room action
+  performAction() {
+    console.log(
+      `Room action performed for room at (${this.xIndex}, ${this.yIndex})`
+    );
+  }
 }
