966eded8f5e416b281ab7a6fcfcdcbed1674b921
muxi <yangqinxue333@163.com>
2024-04-11 11:44:59 +0100

    Add action plan board

--- Room25Web/src/app/actionplan/actionplan.component.ts
+++ Room25Web/src/app/actionplan/actionplan.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { Action } from '../action.enum';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-actionplan',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './actionplan.component.html',
-  styleUrl: './actionplan.component.css'
+  styleUrl: './actionplan.component.css',
 })
 export class ActionplanComponent {
+  action1!: Action;
+  action2!: Action;
+  action3!: Action;
 
+  actionReady: boolean[] = [false, false, false];
+  actionFinished: boolean[] = [false, false, false];
+  action3Enabled: boolean = false;
+
+  enableAction3(enabled: boolean): void {
+    this.action3Enabled = enabled;
+  }
 }
--- Room25Web/src/app/app.component.ts
+++ Room25Web/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 import { GameboardComponent } from './gameboard/gameboard.component';
 import { ActionChooseBoardComponent } from './action-choose-board/action-choose-board.component';
+import { ActionplanComponent } from './actionplan/actionplan.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GameboardComponent, ActionChooseBoardComponent],
+  imports: [RouterOutlet, GameboardComponent, ActionChooseBoardComponent, ActionplanComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
