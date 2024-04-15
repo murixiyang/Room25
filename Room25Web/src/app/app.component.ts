3ba89010ea7e2316c572123c4465afee2ead49a7
lyyang2 <lydia.yang@amadeus.com>
2024-04-15 14:48:42 +0100

    Add timeline

--- Room25Web/src/app/app.component.ts
+++ Room25Web/src/app/app.component.ts
@@ -4,11 +4,18 @@ import { RouterOutlet } from '@angular/router';
 import { GameboardComponent } from './gameboard/gameboard.component';
 import { ActionChooseBoardComponent } from './action-choose-board/action-choose-board.component';
 import { ActionplanComponent } from './actionplan/actionplan.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GameboardComponent, ActionChooseBoardComponent, ActionplanComponent],
+  imports: [
+    RouterOutlet,
+    GameboardComponent,
+    ActionChooseBoardComponent,
+    ActionplanComponent,
+    TimelineComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
